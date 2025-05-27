848245d1a4b4b14744f862363573c38121412baa
eibounda <elvisbetzalel@outlook.fr>
2025-05-27 15:28:53 +0000

    Update install_netcalc-rs.sh

--- scripts/install_netcalc-rs.sh
+++ scripts/install_netcalc-rs.sh
@@ -47,16 +47,16 @@ cd app/
 # Permissions
 echo "[-] Définition des permissions"
 sudo chown -R "$USER":"$USER" .
-sudo chmod +x netcalc-rs
-sudo chmod -R 644 assets
+chmod +x netcalc-rs
+chmod -R 644 assets
 
 # Déplacer les fichiers
 echo "[-] Déplacement des fichiers"
-sudo mkdir -p "$INSTALL_DIR"
+mkdir -p "$INSTALL_DIR"
 
-sudo mv netcalc-rs "$INSTALL_DIR/"
+mv netcalc-rs "$INSTALL_DIR/"
 
-sudo cp -r assets "$INSTALL_DIR/"
+cp -r assets "$INSTALL_DIR/"
 
 # Créer le lien symbolique
 mkdir -p "$BIN_DIR"
@@ -69,8 +69,8 @@ ln -sf "$INSTALL_DIR/netcalc-rs" "$BIN_DIR/netcalc-rs"
 # if [ "$answer" = "o" ]; then
 # echo "[-] Copie des fichiers pour le menu"
 mkdir -p "$DESKTOP_DIR" "$ICON_DIR"
-sudo install -Dm 644 debian/netcalc-rs.desktop "$DESKTOP_DIR/netcalc-rs.desktop"
-sudo install -Dm 644 assets/images/netcalc-rs.png "$ICON_DIR/netcalc-rs.png"
+install -Dm 644 debian/netcalc-rs.desktop "$DESKTOP_DIR/netcalc-rs.desktop"
+install -Dm 644 assets/images/netcalc-rs.png "$ICON_DIR/netcalc-rs.png"
 echo "[+] Application ajoutée au menu"
 # else
 #   echo "[-] L'application sera disponible uniquement en ligne de commande."
