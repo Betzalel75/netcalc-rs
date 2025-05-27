d565aad5749665ebf57f735f9cae1d2d1e7df4c1
eibounda <elvisbetzalel@outlook.fr>
2025-05-27 15:04:10 +0000

    Update install_netcalc-rs.sh

--- scripts/install_netcalc-rs.sh
+++ scripts/install_netcalc-rs.sh
@@ -56,7 +56,7 @@ sudo mkdir -p "$INSTALL_DIR"
 
 sudo mv netcalc-rs "$INSTALL_DIR/"
 
-sudo cp assets "$INSTALL_DIR/"
+sudo cp -r assets "$INSTALL_DIR/"
 
 # Créer le lien symbolique
 mkdir -p "$BIN_DIR"
