9c0898a68823e02f31dcc3bee2c64be428829002
betzalel75 <elvisbetzalel@outlook.fr>
2026-05-30 19:42:31 +0000

    feat(main): add desktop window configuration and conditional launch logic

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
 use netcalc_rs::components::app::App;
 
 fn main() {
-    dioxus::launch(App);
+    #[cfg(feature = "desktop")]
+    {
+        use dioxus::{
+            desktop::{Config, WindowBuilder},
+            LaunchBuilder,
+        };
+
+        let config = Config::new()
+            .with_menu(None)
+            .with_window(WindowBuilder::new().with_title("NetCalc-rs"));
+
+        LaunchBuilder::desktop().with_cfg(config).launch(App);
+    }
+
+    #[cfg(not(feature = "desktop"))]
+    {
+        dioxus::launch(App);
+    }
 }
